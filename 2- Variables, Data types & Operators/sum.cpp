6d4cd726ff09e42f2d3af61fd3424d26bdff5251
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-06-17 13:41:41 +0530

    sum

---
+++ 2- Variables, Data types & Operators/sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int sum;
+    cout << "Enter a:";
+    cin >> a;
+    cout << "Enter b:";
+    cin >> b;
+
+    sum = a + b;
+    cout << "Sum of a and b: " << sum;
+    return 0;
+}
