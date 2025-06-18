7c74b2c86766012f2ade00b2e89cd6ee70600b55
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-06-18 09:55:50 +0530

    forLoops

--- 3-Loops/forLoop.cpp
+++ 3-Loops/forLoop.cpp
@@ -3,12 +3,13 @@ using namespace std;
 int main()
 {
 
-    int n = 10;
+    int n = 5;
+    int sum = 0;
 
     for (int i = 1; i <= n; i++)
     {
-        cout << i;
+        sum = sum + i;
     }
-
+    cout << sum;
     return 0;
 }
