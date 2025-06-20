60eff602774cc5c2066b48932b2cf024597bfb4f
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-06-20 13:51:01 +0530

    sum of 1-N

---
+++ 5-Funtions/question1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sumOfNo(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int main()
+{
+    cout << sumOfNo(5) << endl;
+    return 0;
+}
