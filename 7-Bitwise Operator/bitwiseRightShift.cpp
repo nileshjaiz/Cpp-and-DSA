946bb64a000593605da78e3957536f6af76176cc
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-06-25 10:40:09 +0530

    bitwiswRIghtShift

---
+++ 7-Bitwise Operator/bitwiseRightShift.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 4;
+    // cout << (a >> 1) << endl;        //2
+
+    // int a = 10;
+    // cout << (a >> 2) << endl;        //2
+
+    int a = 21;
+    cout << (a >> 2) << endl; // 5
+
+    return 0;
+}
