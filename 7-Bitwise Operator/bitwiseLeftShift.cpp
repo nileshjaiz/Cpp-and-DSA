90c4751ffef2c12bc556b1a4d4f588e0d817e415
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-06-25 10:27:42 +0530

    bitwiseLeftShift

--- 7-Bitwise Operator/bitwiseLeftShift.cpp
+++ 7-Bitwise Operator/bitwiseLeftShift.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main()
 {
-    int a = 4;
-    cout << (a << 1) << endl;
+    // int a = 4;
+    // cout << (a << 1) << endl;        //8
+
+    int b = 10;
+    cout << (b << 2) << endl; // 40
 
     return 0;
 }
