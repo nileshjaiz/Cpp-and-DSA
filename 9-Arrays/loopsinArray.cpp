834970ed16f76e0e7c485dd0743683c817557893
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-06-25 18:44:30 +0530

    loopsinArray

---
+++ 9-Arrays/loopsinArray.cpp
@@ -0,0 +1,23 @@
+// Loops in Array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int marks[5] = {99, 100, 60, 80, 60};
+    int size = 5;
+
+    // calculting the size of array on our own.
+    //  int sz = sizeof(marks) / sizeof(int);
+    //  cout << sz;
+
+    // loop
+    for (int i = 0; i < size; i++)
+    {
+        cout << marks[i] << endl;
+    }
+
+    return 0;
+}
