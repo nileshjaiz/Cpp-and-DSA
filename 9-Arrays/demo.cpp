f73fe5c650e56a61b31a058d56c119b691a3b922
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-06-25 18:07:36 +0530

    Initializing Array

---
+++ 9-Arrays/demo.cpp
@@ -0,0 +1,17 @@
+// Syntax of array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int marks[5] = {99, 100, 60, 80, 60};
+    cout << marks[0] << endl;
+    cout << marks[4] << endl;
+
+    double price[] = {98.99, 265, 255.87};
+    cout << price[2] << endl;
+
+    return 0;
+}
