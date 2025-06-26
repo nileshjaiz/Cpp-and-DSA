66a4e68df992dd549d75de0fe586872a6ec35fb5
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-06-26 12:03:44 +0530

    reversing an Array

---
+++ 9-Arrays/reversseArr.cpp
@@ -0,0 +1,33 @@
+// Reverse an array usign 2 pointer approach.
+
+#include <iostream>
+using namespace std;
+
+void reverseArr(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+
+    int arr[] = {4, 2, 7, 8, 1, 2, 5};
+    int size = 7;
+
+    reverseArr(arr, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
