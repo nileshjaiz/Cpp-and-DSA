fc7bad07149d17f48ec4502fa4620f5fcecce1c0
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-06-27 12:26:14 +0530

    Maximum Subaaray Sum using Kandanes Algorithm

--- 10-Kadanes Algo/maxSubarraySum2.cpp
+++ 10-Kadanes Algo/maxSubarraySum2.cpp
@@ -1,4 +1,4 @@
-// Kadane's Algorithm | Maximum Sub-Array Sum | Brute Force Approach
+// Kadane's Algorithm | Maximum Sub-Array Sum | Kandane's Algorithm
 
 #include <iostream>
 #include <climits>
@@ -9,16 +9,16 @@ int main()
 
     int arr[7] = {3, -4, 5, 4, -1, 7, -8};
     int n = 7;
-
+    int currSum = 0;
     int maxSum = INT_MIN;
 
     for (int st = 0; st < n; st++)
     {
-        int currSum = 0;
-        for (int end = st; end < n; end++)
+        currSum += arr[st];
+        maxSum = max(currSum, maxSum);
+        if (currSum < 0)
         {
-            currSum += arr[end];
-            maxSum = max(currSum, maxSum);
+            currSum = 0;
         }
     }
 
